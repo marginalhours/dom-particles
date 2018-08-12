a2c57a60ddd79c56fd1b65416961f0280f555004
Glitch (regular-shelf) <none>
2018-08-12 19:51:25 +0000

    🌯👨‍👨‍👦 Checkpoint ./src/scss/stack.scss:728678/1 ./src/js/index.js:728678/128 ./src/js/dialogue.js:728678/1196

---
+++ src/js/dialogue.js
@@ -0,0 +1,19 @@
+import { Hookable } from './helpers';
+
+export class Dialogue extends Hookable {
+  constructor (options) {
+    const { parent } = options;
+    
+    super ({
+      parent,
+      template: `<div class='big-card' data-hook='container'>
+                  <div class='card-image'></div>
+                  <div class='card-image-inner'></div>
+                  <div class='choices'>
+                    <div class='flavour'></div>
+                  </div>
+                 </div>
+                `
+    });
+  }
+}
--- src/js/index.js
+++ src/js/index.js
@@ -1,13 +1,15 @@
 import '../scss/style.scss';
 import { qs } from './helpers';
 import { Bar } from './bar';
-import { EventList} from './event-list';
+import { EventList } from './event-list';
+import { Dialogue } from './dialogue'; 
 
-let h = new Bar({parent: qs('body'), name: "health"});
-let k = new Bar({parent: qs('body'), name: "mana"});
-let e = new Bar({parent: qs('body'), name: "experience"});
+let h = new Bar({ parent: qs('body'), name: "health" });
+let k = new Bar({ parent: qs('body'), name: "mana" });
+let e = new Bar({ parent: qs('body'), name: "experience" });
 
-let s = new EventList({parent: qs('body')});
+let s = new EventList({ parent: qs('body') });
+let d = new Dialogue({ parent: qs('body') });
 
 s.add();
 s.add();
@@ -17,7 +19,7 @@ s.add();
 h.setText("15/30 HP");
 k.setText("10/30 MP");
 e.setText("100 EXP"); 
- 
+
 h.setPercentage(80);
 k.setPercentage(90);
 e.setPercentage(50);
