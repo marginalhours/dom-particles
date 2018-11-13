c83d9b281281d0b2a3fa6031992882259caaac4f
Glitch (cogent-baboon) <none>
2018-11-13 20:19:50 +0000

    🚈🚅 Checkpoint ./src/js/index.js:728678/947

--- src/js/index.js
+++ src/js/index.js
@@ -7,26 +7,28 @@ const GRAVITY = 0.1;
 
 document.querySelector('button').addEventListener('click', () => {
   t.createEmitter({
-    position: { x: 100, y: document.body.clientHeight / 2},
+    position: { x: document.body.clientWidth / 2 - 50, y: document.body.clientHeight / 2},
     velocity: { x: 0, y: 100 },
     emitEvery: 10,
+    onUpdate: (emitter) => {
+      let h = Math.atan2(c.y - emitter.position.y, c.x - emitter.position.x);
+      let k = 100;
+      emitter.acceleration = { x: k * Math.cos(h), y: k * Math.sin(h) }
+    },
     getParticleTTL: () => 2000 + 1000 * Math.random(),
     getParticleText: () => ['#', '!', ',', '.', '$', '%'][Math.floor(6 * Math.random())],
-    getParticleVelocity: () => {
+    getParticleVelocity: (emitter) => {
       let k = 150 + 50 * Math.random();  
-      let theta = ((2/6) * Math.PI * (Math.random() - 0.5));
-      return {x: k * Math.cos(theta), y: k * Math.sin(theta)};
+      let h = Math.atan2(c.y - emitter.position.y, c.x - emitter.position.x);
+      h += (1/12) * Math.PI * (Math.random() - 0.5);
+      return {x: k * Math.cos(h), y: k * Math.sin(h)};
     },
     // getAcceleration: () => ({x: 0, y: 50}),
     onParticleCreate: (p) => {
      p.setStyle({ fontSize: 14, color: '#aaa', width: '12px', }); 
     },
     onParticleUpdate: (p) => {
-      // p.setStyle({ opacity: p.lerp(1, 0)});
-      let h = Math.atan2(c.y - p.position.y, c.x - p.position.x);
-      let k = Math.sqrt((c.x - p.position.x)*(c.x - p.position.x) + (c.y - p.position.y)*(c.y - p.position.y));
-      
-      p.acceleration = { x: -GRAVITY * k * Math.cos(h), y: -GRAVITY * k * Math.sin(h) }
+      p.setStyle({ opacity: p.lerp(1, 0)});
     }
   });
 });
