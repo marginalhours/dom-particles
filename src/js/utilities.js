021e886fbf123ded1c3e3d97b7e8081ab4a0f0aa
Glitch (cogent-baboon) <none>
2018-11-14 21:09:20 +0000

    🍘🎅 Checkpoint ./src/js/utilities.js:728678/1229

--- src/js/utilities.js
+++ src/js/utilities.js
@@ -1,3 +1,4 @@
+/* Useful regexes */
 const RGB_PATTERN = /rgb\(\s*(\d+)\s*,\s*(\d+)\s*,\s*(\d+)\s*\)/;
 const RGBA_PATTERN = /rgba\(\s*(\d+)\s*,\s*(\d+)\s*,\s*(\d+)\s*,\s*([01](?:\.\d+)*)\s*\)/;
 const HEX_PATTERN = /#([0-9a-f]{1,2})([0-9a-f]{1,2})([0-9a-f]{1,2})/;
@@ -6,7 +7,7 @@ const NUMBER_AND_UNIT_PATTERN = /(\d+|\d+\.\d+)([a-z]+)/;
 export const rgbToNumbers = (string) => {
   try {
     let [_, r, g, b] = RGB_PATTERN.exec(string);
-    return [r, g, b].map(v => parseInt(v)) 
+    return [...[r, g, b].map(v => parseInt(v)), 1.0]; 
   }
   catch (err){
     return false;
@@ -25,29 +26,26 @@ export const rgbaToNumbers = (string) => {
 
 export const hexToNumbers = (string) => {
   try {
-    let [_, r, g, b]   
-  }
-}
-
-export const colourFromRGB = ([r, g, b, a]
-export const colourFromRGBA = (r, g, b, a) => `rgba(${r}, ${g}, ${b}, ${a || 1.0}`;
-
-export const lerp = (a, b, frac) => a + ((b - a) * frac);
-
-
-export const hexToNumbers = (hex) => {
-  let h = [...hex.substring(1)];
-  if (h.length === 3) {
-    return h.map(d => parseInt(d, 16))  
-  } else {
-    return [0, 2, 4].map(ix => parseInt(`${h[ix]}${h[ix + 1]}`, 16));  
+    let [_, r, g, b] = HEX_PATTERN.exec(string);
+    return [...[r, g, b].map(x => parseInt(x, 16) * ((x.length === 1) ? 0x11 : 0x1)), 1.0];
+  } 
+  catch (err) {
+    return false;  
   }
 }
 
 export const extractNumberAndUnit = (string) => {
   let [_, num, unit] = NUMBER_AND_UNIT_PATTERN.exec(string);
-  return { value: parseInt(num), unit }
+  return [parseInt(num), unit]
 }
+
+
+/* Take a colour array, turn it back into CSS */
+export const colourToCSSString = ([r, g, b, a]) => `rgba(${r}, ${g}, ${b}, ${a})`;
+
+/* Easing functions */
+export const lerp = (a, b, frac) => a + ((b - a) * frac);
+
 
 export const easeArray = (array, easeFn, frac) => {
     let idxFrac = 1 / array.length;
