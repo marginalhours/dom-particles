2f630d7c62cf64bf1cabbde79a5fec66043e81b7
Glitch (cogent-baboon) <none>
2018-11-14 20:36:20 +0000

    🐁👟 Checkpoint ./src/js/utilities.js:728678/1453

--- src/js/utilities.js
+++ src/js/utilities.js
@@ -14,3 +14,16 @@ export const hexToNumbers = (hex) => {
     return [0, 2, 4].map(ix => parseInt(`${h[ix]}${h[ix + 1]}`, 16));  
   }
 }
+
+export const extractNumberAndUnit = (string) => {
+  let [_, num, unit] = /(\d+|\d+\.\d+)([a-z]+)/.exec(string);
+  return { value: parseInt(num), unit }
+}
+
+export const easeArray = (array, easeFn, frac) {
+    let idxFrac = 1 / array.length;
+    let idx = Math.round(frac / idxFrac);
+    let nextIdx = idx === array.length - 1 ? idx : idx + 1;
+    return easeFn(array[idx], array[nextIdx], frac);
+}
+  
