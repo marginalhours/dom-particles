2db54ac797146ae16b75e59f1614af85c554bcaf
Glitch (cogent-baboon) <none>
2018-11-17 13:30:02 +0000

    🌎💘 Checkpoint ./webpack.config.js:728678/168

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,9 @@ const path = require('path'),
       html = require('html-webpack-plugin'),
       webpack = require('webpack');
 
+const library = 'library';
+const filename = `${library}.js
+
 module.exports = {
   entry: {
     index: './src/js/index.js'
@@ -18,6 +21,9 @@ module.exports = {
     filename: '[name].js',
     path: path.resolve(__dirname, 'dist'),
     publicPath: '',
+    library: libraryName,
+    libraryTarget: 'umd',
+    umdNamedDefine: true
   },
   module: {
     rules: [
