34dfabbd8007e4a2f398c12ef11e1aa09b11e82f
Glitch (cogent-baboon) <none>
2018-11-21 11:24:03 +0000

    🚗🚆 Checkpoint ./package.json:728678/107 ./test/utilities.spec.js:728678/383

---
+++ test/utilities.spec.js
@@ -0,0 +1,10 @@
+var assert = require('assert');
+var utilities = require('../src/js/utilities');
+
+describe('Array', function() {
+  describe('#indexOf()', function() {
+    it('should return -1 when the value is not present', function() {
+      assert.equal([1,2,3].indexOf(4), -1);
+    });
+  });
+});
