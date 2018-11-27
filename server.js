52c0a4b6d28b29ef4fc5f035593220c8fa75079e
Glitch (cogent-baboon) <none>
2018-11-27 08:22:04 +0000

    🛫🐢 Checkpoint ./examples/index.html:728678/2671 ./server.js:728678/153 ./examples/index.css:728678/341 ./.trigger-rebuild:728678/2

--- server.js
+++ server.js
@@ -1,15 +1,19 @@
 var express = require('express');
 var app = express();
 
-app.use(express.static('dist'));
+app.use(express.static('examples'));
 
-app.get("/", (request, response) => {
-  response.sendFile(__dirname + '/examples/index.html');
-});
+// app.get("/", (request, response) => {
+//   response.sendFile(__dirname + '/examples/index.html');
+// });
 
-app.get("/index.js", (request, response) => {
-  response.sendFile(__dirname + '/examples/index.js');
-});
+// app.get("/index.js", (request, response) => {
+//   response.sendFile(__dirname + '/examples/index.js');
+// });
+
+// app.get("/index.css", (request, response) => {
+//   response.sendFile(__dirname + '/examples/index.css');
+// });
 
 app.get("/lib/letterbomb.js", (request, response) => {
   response.sendFile(__dirname + '/lib/letterbomb.js');
